e28cd9e3db542cdb290c75117c3ecc99ac67508b
Emilasdasd <johanssone668@gmail.com>
2023-09-15 08:40:28 +0200

    req fil

--- sentiment_swe.py
+++ sentiment_swe.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 model = AutoModelForSequenceClassification.from_pretrained("model/KBLab/robust-swedish-sentiment-multiclass")
 tokenizer = AutoTokenizer.from_pretrained("model/KBLab/robust-swedish-sentiment-multiclass")
 
-#a
+
 async def swe_review_rating(input):
     tokens = tokenizer.encode(input, return_tensors="pt")
     result = model(tokens)
